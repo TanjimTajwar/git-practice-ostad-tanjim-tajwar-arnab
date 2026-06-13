9f5a017b7515567f1b29867bfc92ae1d2760c020
tanjimtajwar <tanjimtajwar@yahoo.com>
2026-06-13 23:03:09 +0600

    Add multiply function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from utils import add, subtract
+from utils import add, subtract,multiply
 
 print("Name: Tanjim Tajwar Arnab")
 print("Date:", datetime.today().date())
 
 print("Addition:", add(5, 3))
 print("Subtraction:", subtract(5, 3))
+print("Multiply:", multiply(5, 3))
