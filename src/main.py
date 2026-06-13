1b5b165f8d7d1989a80f4cb970bebb1d90e2df00
tanjimtajwar <tanjimtajwar@yahoo.com>
2026-06-13 22:58:20 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from datetime import datetime
+from utils import add, subtract
 
 print("Name: Tanjim Tajwar Arnab")
 print("Date:", datetime.today().date())
+
+print("Addition:", add(5, 3))
+print("Subtraction:", subtract(5, 3))
